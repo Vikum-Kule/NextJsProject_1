b48fa9e8c0ac8e08d4c15486ff316b151da94235
BI-Project-Enactor <vikumkulatunga@gmail.com>
2022-09-11 12:39:29 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About Page</h1>
+            <p>bahygfwyegfwey bhbwev uhd efhwe  eufhweufafwefwuh </p>
+        </div>
+     );
+}
+ 
+export default About;
